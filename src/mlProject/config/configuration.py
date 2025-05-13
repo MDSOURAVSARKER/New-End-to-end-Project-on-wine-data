5ceb1d3963f894530e5f8c2cbe6809f9610585e4
MD SOURAV SARKER <souravsarker.ce@gmail.com>
2025-05-13 22:47:37 +0800

    cpmpleted data ingestion

---
+++ src/mlProject/config/configuration.py
@@ -0,0 +1,56 @@
+
+from unittest import TestCase  # Built-in, no pip install needed
+TestCase.assertRaisesRegex  # <-- This is what you use
+import unittest
+from pathlib import Path
+
+class TestYAML(unittest.TestCase):
+    def test_empty(self):
+        self.assertRaisesRegex(ValueError, "yaml file is empty", read_yaml, Path("empty.yaml"))
+from mlProject.constants import *
+from mlProject.utils.common import read_yaml, create_directories
+
+from mlProject.entity.config_entity import DataIngestionConfig
+
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH):
+
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+
+        create_directories([config.root_dir])
+
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir 
+        )
+
+        return data_ingestion_config
+    
+    
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+
+        create_directories([config.root_dir])
+
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir 
+        )
+
+        return data_ingestion_config
