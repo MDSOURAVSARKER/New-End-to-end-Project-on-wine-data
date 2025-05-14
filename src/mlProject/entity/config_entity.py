3725d9c3be2351933450546911301872b34981bb
MD SOURAV SARKER <souravsarker.ce@gmail.com>
2025-05-14 22:54:30 +0800

    model trainer added

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -9,10 +9,6 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
     
-    
-    from dataclasses import dataclass
-from pathlib import Path
-
 
 @dataclass(frozen=True)
 class DataValidationConfig:
@@ -20,3 +16,22 @@ class DataValidationConfig:
     STATUS_FILE: str
     unzip_data_dir: Path
     all_schema: dict
+    
+
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    
+    
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    alpha: float
+    l1_ratio: float
+    target_column: str
